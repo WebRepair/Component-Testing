9ef4f53c3dccfec8df838fba591d40249e1ab238
WebRepair <108232179+WebRepair@users.noreply.github.com>
2023-05-18 21:47:32 +0200

    updated ui + everything Jsonated

---
+++ buttons.js
@@ -0,0 +1,142 @@
+// nine BUTTON
+
+
+
+
+const toggleButton9 = document.getElementById('nineButton') as HTMLElement;
+const firstComponent9 = document.querySelector('.nineComponent') as HTMLElement;
+
+
+// Define a function to handle the click event
+function toggleComponent9() {
+if (firstComponent9?.style.display === 'none') {
+  firstComponent9.style.display = 'block'; // Show the component
+  toggleButton9.style.backgroundColor = '';
+} else {
+  firstComponent9.style.display = 'none'; // Hide the component
+  toggleButton9.style.backgroundColor = 'grey'; // Hide the component
+}
+}
+
+
+// Attach the click event to the button
+toggleButton9.addEventListener('click', toggleComponent9);
+
+
+
+
+
+// ten BUTTON
+
+
+
+
+const toggleButton10 = document.getElementById('tenButton') as HTMLElement;
+const firstComponent10 = document.querySelector('.tenComponent') as HTMLElement;
+
+
+// Define a function to handle the click event
+function toggleComponent10() {
+if (firstComponent10?.style.display === 'none') {
+  firstComponent10.style.display = 'block'; // Show the component
+  toggleButton10.style.backgroundColor = '';
+} else {
+  firstComponent10.style.display = 'none'; // Hide the component
+  toggleButton10.style.backgroundColor = 'grey'; // Hide the component
+}
+}
+
+
+// Attach the click event to the button
+toggleButton10.addEventListener('click', toggleComponent10);
+
+
+// ELEVEN BUTTON
+const toggleButton11 = document.getElementById('elevenButton') as HTMLElement;
+const firstComponent11 = document.querySelector('.elevenComponent') as HTMLElement;
+
+// Define a function to handle the click event
+function toggleComponent11() {
+  if (firstComponent11?.style.display === 'none') {
+    firstComponent11.style.display = 'block'; // Show the component
+    toggleButton11.style.backgroundColor = '';
+  } else {
+    firstComponent11.style.display = 'none'; // Hide the component
+    toggleButton11.style.backgroundColor = 'grey';
+  }
+}
+
+// Attach the click event to the button
+toggleButton11.addEventListener('click', toggleComponent11);
+
+// TWELVE BUTTON
+const toggleButton12 = document.getElementById('twelveButton') as HTMLElement;
+const firstComponent12 = document.querySelector('.twelveComponent') as HTMLElement;
+
+// Define a function to handle the click event
+function toggleComponent12() {
+  if (firstComponent12?.style.display === 'none') {
+    firstComponent12.style.display = 'block'; // Show the component
+    toggleButton12.style.backgroundColor = '';
+  } else {
+    firstComponent12.style.display = 'none'; // Hide the component
+    toggleButton12.style.backgroundColor = 'grey';
+  }
+}
+
+// Attach the click event to the button
+toggleButton12.addEventListener('click', toggleComponent12);
+
+// THIRTEEN BUTTON
+const toggleButton13 = document.getElementById('thirteenButton') as HTMLElement;
+const firstComponent13 = document.querySelector('.thirteenComponent') as HTMLElement;
+
+// Define a function to handle the click event
+function toggleComponent13() {
+  if (firstComponent13?.style.display === 'none') {
+    firstComponent13.style.display = 'block'; // Show the component
+    toggleButton13.style.backgroundColor = '';
+  } else {
+    firstComponent13.style.display = 'none'; // Hide the component
+    toggleButton13.style.backgroundColor = 'grey';
+  }
+}
+
+// Attach the click event to the button
+toggleButton13.addEventListener('click', toggleComponent13);
+
+// FOURTEEN BUTTON
+const toggleButton14 = document.getElementById('fourteenButton') as HTMLElement;
+const firstComponent14 = document.querySelector('.fourteenComponent') as HTMLElement;
+
+// Define a function to handle the click event
+function toggleComponent14() {
+  if (firstComponent14?.style.display === 'none') {
+    firstComponent14.style.display = 'block'; // Show the component
+    toggleButton14.style.backgroundColor = '';
+  } else {
+    firstComponent14.style.display = 'none'; // Hide the component
+    toggleButton14.style.backgroundColor = 'grey';
+  }
+}
+
+// Attach the click event to the button
+toggleButton14.addEventListener('click', toggleComponent14);
+
+// FIFTEEN BUTTON
+const toggleButton15 = document.getElementById('fifteenButton') as HTMLElement;
+const firstComponent15 = document.querySelector('.fifteenComponent') as HTMLElement;
+
+// Define a function to handle the click event
+function toggleComponent15() {
+  if (firstComponent15?.style.display === 'none') {
+    firstComponent15.style.display = 'block'; // Show the component
+    toggleButton15.style.backgroundColor = '';
+  } else {
+    firstComponent15.style.display = 'none'; // Hide the component
+    toggleButton15.style.backgroundColor = 'grey';
+  }
+}
+
+// Attach the click event to the button
+toggleButton15.addEventListener('click', toggleComponent15);
